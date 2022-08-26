6f3a8f9d5920f9b9b2e3bca26ebb0ad0fc804726
unknown <sanjog.analogue51@gmail.com>
2022-08-26 11:41:36 +0545

    asdas

---
+++ Home.js
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import tw from 'tailwind-react-native-classnames';
+
+const Home = () => {
+  return (
+    <View >
+      <Text style={tw`bg-blue-100 text-white pt-4`}>Open up Apps.js to start working on your app!</Text>
+      
+    </View>
+  )
+}
+
+
+
+export default Home
